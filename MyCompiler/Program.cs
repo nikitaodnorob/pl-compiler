5cf194315d11068b0931671e63003bcd956a5c01
nikitaodnorob <nikitaodnorob@gmail.com>
2020-03-20 22:35:28 +0300

    Connect Roslyn's packages for compiling empty programs

--- MyCompiler/Program.cs
+++ MyCompiler/Program.cs
@@ -4,11 +4,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using MyCompiler.Visitors;
+using System.IO;
 
 namespace MyCompiler
 {
     class Program
     {
+        /// <summary>
+        /// Compile the program which is sets by syntax tree
+        /// </summary>
+        /// <param name="syntaxTree">Syntax tree</param>
+        static void Compile(Node syntaxTree)
+        {
+            //initialize visitor
+            RoslynTreeBuilderVisitor visitor = new RoslynTreeBuilderVisitor();
+
+            //run visitor
+            syntaxTree.Visit(visitor);
+
+            //get unit which prepared for compilation
+            var programUnit = visitor.UnitNode;
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                "assemblyName",
+                new[] { programUnit.SyntaxTree },
+                new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) },
+                new CSharpCompilationOptions(
+                    OutputKind.ConsoleApplication, //set application type as console app
+                    true, //report about suppressed errors and warnings
+                    null, //default module name
+                    null, //set that any static Main method is may be entrypoint
+                    null,
+                    null, //set empty list of usings
+                    OptimizationLevel.Release //set optimization level for Release mode
+                )
+            );
+
+            //generate program to "program.exe" file
+            using (var exeStream = new FileStream("../../program.exe", FileMode.Create))
+            {
+                var emitResult = compilation.Emit(exeStream); //compile
+
+                //if we have warnings, print them
+                foreach (var error in emitResult.Diagnostics.Where(diagnostic => diagnostic.WarningLevel > 0))
+                    Console.WriteLine(error);
+
+                if (!emitResult.Success)
+                {
+                    //if we have errors, print them
+                    foreach (var error in emitResult.Diagnostics.Where(diagnostic => diagnostic.WarningLevel == 0))
+                        Console.WriteLine(error);
+                }
+            }
+
+            //print C# source code which matches to builded Roslyn's syntax tree
+            Console.WriteLine("========== SOURCE CODE ==========");
+            Console.WriteLine(programUnit.NormalizeWhitespace().ToFullString());
+        }
+
         static void Main(string[] args)
         {
             //set culture for correct double values parsing
@@ -30,6 +86,7 @@ print 100;
             if (isCorrect) //if program was parsed successfully
             {
                 Node syntaxTree = parser.Root; //get program's syntax tree
+                Compile(syntaxTree); //compile the program
             }
         }
     }
---
+++ MyCompiler/Visitors/RoslynTreeBuilderVisitor.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MyCompiler.Visitors
+{
+    class RoslynTreeBuilderVisitor : BaseVisitor
+    {
+        /// <summary>
+        /// Stack of expressions for storing expressions' parts
+        /// </summary>
+        private Stack<ExpressionSyntax> expressions = new Stack<ExpressionSyntax>();
+
+        /// <summary>
+        /// Stack of program blocks for remembering what block is current
+        /// </summary>
+        private Stack<SyntaxNode> blocks = new Stack<SyntaxNode>();
+
+        /// <summary>
+        /// Auxiliary function for generating tree node of Using syntax
+        /// </summary>
+        /// <param name="usingName">Name of using</param>
+        /// <returns>Using syntax node</returns>
+        private UsingDirectiveSyntax CreateUsingDirective(string usingName)
+        {
+            NameSyntax qualifiedName = null;
+
+            foreach (var identifier in usingName.Split('.'))
+            {
+                var name = SyntaxFactory.IdentifierName(identifier);
+
+                if (qualifiedName != null) qualifiedName = SyntaxFactory.QualifiedName(qualifiedName, name);
+                else qualifiedName = name;
+            }
+
+            return SyntaxFactory.UsingDirective(qualifiedName);
+        }
+
+        /// <summary>
+        /// Auxiliary function for adding the statement into current block (it is in the top of blocks' stack)
+        /// </summary>
+        /// <param name="statement">Node of statement</param>
+        private void AddStatementToCurrentBlock(StatementSyntax statement)
+        {
+            //look current block
+            SyntaxNode currentBlock = blocks.Peek();
+
+            //if statement isn't in any block, it's need to add the statement to Main method in C#
+            if (currentBlock is MethodDeclarationSyntax && (currentBlock as MethodDeclarationSyntax).Identifier.Text == "Main")
+                mainMethodNode = mainMethodNode.AddBodyStatements(statement);
+
+            //if we forget some type of block, we will get an exception
+            else throw new Exception($"current block was {currentBlock.GetType()}");
+        }
+
+        /// <summary>
+        /// Node of C# unit which contains all classes
+        /// </summary>
+        private CompilationUnitSyntax unitNode;
+
+        /// <summary>
+        /// Node of C# class which contains Main method
+        /// </summary>
+        private ClassDeclarationSyntax programClassNode;
+
+        /// <summary>
+        /// Node of C# Main method
+        /// </summary>
+        private MethodDeclarationSyntax mainMethodNode;
+
+        /// <summary>
+        /// Ready C# unit node which prepared for compilation
+        /// </summary>
+        public CompilationUnitSyntax UnitNode
+        {
+            get
+            {
+                //add C# Main method into class
+                programClassNode = programClassNode.AddMembers(mainMethodNode);
+
+                //declare namespace and add the class into it 
+                NamespaceDeclarationSyntax namespaceNode = SyntaxFactory
+                    .NamespaceDeclaration(SyntaxFactory.IdentifierName("RoslynApp"))
+                    .AddMembers(programClassNode);
+
+                //add namespace into unit 
+                unitNode = unitNode.AddMembers(namespaceNode);
+
+                return unitNode;
+            }
+            private set { unitNode = value; }
+        }
+
+        /// <summary>
+        /// Initializing of visitor
+        /// </summary>
+        public RoslynTreeBuilderVisitor()
+        {
+            //Create unit and add using "System"
+            unitNode = SyntaxFactory.CompilationUnit().AddUsings(
+                CreateUsingDirective("System")
+            );
+
+            //Create public class "Program"
+            programClassNode = SyntaxFactory
+                    .ClassDeclaration("Program")
+                    .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+
+            //Create method void Main() 
+            mainMethodNode = SyntaxFactory.MethodDeclaration(
+                SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.VoidKeyword)),
+                "Main"
+            );
+
+            //Set Main method as static
+            mainMethodNode = mainMethodNode.AddModifiers(SyntaxFactory.Token(SyntaxKind.StaticKeyword));
+
+            //Add to Main method empty body
+            mainMethodNode = mainMethodNode.AddBodyStatements();
+
+            //Set Main method as current block (push to stack)
+            blocks.Push(mainMethodNode);
+        }
+    }
+}
